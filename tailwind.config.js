a4d7d7766c70d7ea345e644c45dbd663e450d3f6
Anggito Wicaksono <anggitov@gmail.com>
2024-05-01 23:21:40 +0700

    setup project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        'dark-gray': 'hsl(0, 0%, 59%)',
+        'very-dark-gray': 'hsl(0, 0%, 17%)'
+      },
+      fontFamily: {
+        rubik: ['Rubik', 'sans-serif']
+      }
+    }
+  },
+  plugins: []
+};
